3c186d4de878f2d6c5cd05cb4720db1916a175fe
lohran-mendes <lohranfm@gmail.com>
2024-06-09 16:48:54 -0300

    feat: make user.component dinamic

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent {
+  @Input() avatar!: string;
+  @Input() name!: string;
 
+  get avatarPath() {
+    return './assets/users/' + this.avatar;
+  }
+
+  get avatarAlt() {
+    return 'Imagem de perfil de ' + this.name;
+  }
 }
